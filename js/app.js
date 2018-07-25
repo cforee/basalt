b959ba6da41601ad904e781bc2cbe299d7324e11
cforee <cforee@gmail.com>
2018-07-25 16:00:43 -0700

    Unregister pressed key list when leaving window

--- js/app.js
+++ js/app.js
@@ -7,12 +7,6 @@ World = function(map_name) {
   this.map_name = map_name;
 };
 
-Actions = function() {
-  this.move = function(direction) {
-    console.log(direction);
-  }
-}
-
 KEYS = {
   '38': {
     human_friendly_name: 'up arrow',
@@ -38,16 +32,21 @@ KEYS = {
 
 Basalt = function() {
   var self = this;
-  this.player = new Sprite(5,5);
-  this.world = new World('starting_area');
-  this.actions = new Actions();
-  this.keys = $.extend(KEYS, { pressed: [] });
+  self.player = new Sprite(5,5);
+  self.world = new World('starting_area');
+  self.actions = {
+    move: function(opts) {
+      console.log('moving ', opts.direction);
+    },
+  };
 
-  this.register_keypress = function(key_code) {
+  self.keys = $.extend(KEYS, { pressed: [] });
+
+  self.register_keypress = function(key_code) {
     return ($.inArray(key_code, self.keys.pressed) < 0) ? self.keys.pressed.push(key_code) : false;
   };
 
-  this.unregister_keypress = function(key_code) {
+  self.unregister_keypress = function(key_code) {
     while (self.keys.pressed.indexOf(key_code) >= 0) {
       self.keys.pressed.splice(self.keys.pressed.indexOf(key_code), 1);
     }
@@ -58,16 +57,17 @@ Basalt = function() {
     var pressed_key_index = self.keys.pressed.length;
     while(pressed_key_index--) {
       if (KEYS[self.keys.pressed[pressed_key_index]]) {
-        f = 'self.actions.' + KEYS[self.keys.pressed[pressed_key_index]].action;
+        action = 'self.actions.' + KEYS[self.keys.pressed[pressed_key_index]].action;
         opts = KEYS[self.keys.pressed[pressed_key_index]].opts;
-        eval(f)(opts);
+        eval(action)(opts);
       }
     }
-  }, 1);
+  }, 0);
 
   // DOM events
   $(document).on('keydown', function(e) { self.register_keypress(e.keyCode) });
   $(document).on('keyup', function(e) { self.unregister_keypress(e.keyCode) });
+  $(document).on('mouseleave', function(e) { self.keys.pressed = [] });
 
 };
 
